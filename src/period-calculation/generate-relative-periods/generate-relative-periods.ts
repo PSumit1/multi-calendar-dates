28b675ea2aa9ccb17929ba6a1c2a95f87e1036ac
Gift Nnko <nnkogift@gmail.com>
2025-11-27 08:12:55 +0300

    chore: refactored period relative generator functions to a new file

--- src/period-calculation/generate-relative-periods/generate-relative-periods.ts
+++ src/period-calculation/generate-relative-periods/generate-relative-periods.ts
@@ -1,7 +1,8 @@
-import { formatDate, getNowInCalendar } from "../../utils"
-import { PeriodType, RelativePeriod } from "../types"
 import i18n from '@dhis2/d2-i18n'
 import { Temporal } from '@js-temporal/polyfill'
+import { formatDate, getNowInCalendar } from '../../utils'
+import { PeriodType, RelativePeriod } from '../types'
+import { getMonthsPeriodType } from './constants'
 
 type GenerateFixedPeriods = (options: {
     // year: number
@@ -14,57 +15,28 @@ type GenerateFixedPeriods = (options: {
     // endsBefore?: string
 }) => Array<RelativePeriod>
 
-
-const getMonthsPeriodType = () => [
-    { id: 'THIS_MONTH', name: i18n.t('This month'), offset: 0, duration: 1 },
-    { id: 'LAST_MONTH', name: i18n.t('Last month'), offset: -1, duration: 1 },
-    {
-        id: 'LAST_3_MONTHS',
-        name: i18n.t('Last 3 months'),
-        offset: -1,
-        duration: 3,
-    },
-    {
-        id: 'LAST_6_MONTHS',
-        name: i18n.t('Last 6 months'),
-        offset: -1,
-        duration: 6,
-    },
-    {
-        id: 'LAST_12_MONTHS',
-        name: i18n.t('Last 12 months'),
-        offset: -1,
-        duration: 12,
-    },
-    {
-        id: 'MONTHS_THIS_YEAR',
-        name: i18n.t('Months this year'),
-        offset: 11,
-        duration: 12,
-    },
-]
-
 const generateRelativePeriods: GenerateFixedPeriods = ({
-                                                           periodType,
-                                                           referenceDate // string or Temporal?
-                                                       }) => {
+    periodType,
+    referenceDate, // string or Temporal?
+}) => {
+    const date = referenceDate
+        ? Temporal.PlainDate.from(referenceDate)
+        : getNowInCalendar()
 
-    const date = referenceDate ? Temporal.PlainDate.from(referenceDate) : getNowInCalendar()
-
-    if(periodType === "MONTHLY") {
-        const result = getMonthsPeriodType().map(periodTypeConfig => {
-            const endDate = date.add({months: periodTypeConfig.offset}).with({day: 31});
-            const startDate = date.add({months: periodTypeConfig.offset}).with({day: 1})
-            console.log({
-                endDate: endDate.toString(),
-                startDate: startDate.toString()
-            })
+    if (periodType === 'MONTHLY') {
+        const result = getMonthsPeriodType().map((periodTypeConfig) => {
+            const endDate = date
+                .add({ months: periodTypeConfig.offset })
+                .with({ day: 31 })
+            const startDate = date
+                .add({ months: periodTypeConfig.offset })
+                .with({ day: 1 })
             return {
                 ...periodTypeConfig,
-                periodType: "MONTHLY" as const,
+                periodType: 'MONTHLY' as const,
                 displayName: periodTypeConfig.name,
                 startDate: formatDate(startDate),
-                endDate: formatDate(endDate)
+                endDate: formatDate(endDate),
             }
         })
         return result
@@ -72,7 +44,7 @@ const generateRelativePeriods: GenerateFixedPeriods = ({
 
     // do same for other period types
 
-    throw "not implemented"
+    throw 'not implemented'
 }
 
 export default generateRelativePeriods
