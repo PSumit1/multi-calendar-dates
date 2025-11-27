d3f0fd1a7de25610257f65e39fb1e8d7c5f974e5
Gift Nnko <nnkogift@gmail.com>
2025-11-27 08:09:09 +0300

    chore: added tests for relative monthly periods

--- src/period-calculation/generate-relative-periods/generate-relative-periods.spec.ts
+++ src/period-calculation/generate-relative-periods/generate-relative-periods.spec.ts
@@ -1,39 +1,102 @@
-import generateRelativePeriods from './generate-relative-periods'
 import { Temporal } from '@js-temporal/polyfill'
+import { getNowInCalendar } from '../../utils'
+import generateRelativePeriods from './generate-relative-periods'
 // Start with Gregorian
 // Write tests for one period type at a time
 // Implement with most naive implementation then refactor
 // something very similar to: https://github.com/dhis2/analytics/blob/master/src/components/PeriodDimension/utils/relativePeriods.js#L328
 
-
 /*
-* @nnkogift will work on DAILY,MONTHLY,WEEKLY,
-* @PSummit will work on QUARTERLY, YEARLY
-* */
+ * @nnkogift will work on DAILY,MONTHLY,WEEKLY,
+ * @PSummit will work on QUARTERLY, YEARLY
+ * */
 
 beforeEach(() => {
     // 25 November 2025
-    jest.spyOn(Date, 'now').mockReturnValue(1764069336553)
+    jest.fn(getNowInCalendar).mockReturnValue(
+        Temporal.ZonedDateTime.from({
+            year: 2025,
+            month: 11,
+            day: 25,
+            timeZone: 'UTC',
+        })
+    )
 })
 
 const monthlyDates = [
     {
-        id: "THIS_MONTH",
-        name: "This month",
+        id: 'THIS_MONTH',
+        name: 'This month',
         startDate: Temporal.PlainDate.from({
             year: 2025,
-            month: 2,
+            month: 11,
             day: 1,
         }).toString(),
         endDate: Temporal.PlainDate.from({
             year: 2025,
-            month: 2,
-            day: 28,
-        }).toString()
+            month: 11,
+            day: 30,
+        }).toString(),
     },
     {
-        id: "LAST_MONTH",
-        name: "Last month",
+        id: 'LAST_MONTH',
+        name: 'Last month',
+        startDate: Temporal.PlainDate.from({
+            year: 2025,
+            month: 10,
+            day: 1,
+        }).toString(),
+        endDate: Temporal.PlainDate.from({
+            year: 2025,
+            month: 10,
+            day: 31,
+        }).toString(),
+    },
+    {
+        id: 'LAST_3_MONTHS',
+        name: 'Last 3 months',
+        startDate: Temporal.PlainDate.from({
+            year: 2025,
+            month: 8,
+            day: 1,
+        }).toString(),
+        endDate: Temporal.PlainDate.from({
+            year: 2025,
+            month: 10,
+            day: 31,
+        }).toString(),
+    },
+    {
+        id: 'LAST_6_MONTHS',
+        name: 'Last 6 months',
+        startDate: Temporal.PlainDate.from({
+            year: 2025,
+            month: 5,
+            day: 1,
+        }).toString(),
+        endDate: Temporal.PlainDate.from({
+            year: 2025,
+            month: 10,
+            day: 31,
+        }).toString(),
+    },
+    {
+        id: 'LAST_12_MONTHS',
+        name: 'Last 12 months',
+        startDate: Temporal.PlainDate.from({
+            year: 2024,
+            month: 11,
+            day: 1,
+        }).toString(),
+        endDate: Temporal.PlainDate.from({
+            year: 2025,
+            month: 10,
+            day: 31,
+        }).toString(),
+    },
+    {
+        id: 'MONTHS_THIS_YEAR',
+        name: 'Months this year',
         startDate: Temporal.PlainDate.from({
             year: 2025,
             month: 1,
@@ -41,28 +104,32 @@ const monthlyDates = [
         }).toString(),
         endDate: Temporal.PlainDate.from({
             year: 2025,
-            month: 1,
+            month: 12,
             day: 31,
-        }).toString()
+        }).toString(),
     },
-
 ]
 
 describe('MONTHLY relative periods', () => {
     const periods = generateRelativePeriods({
         periodType: 'MONTHLY',
-        referenceDate: "2025-02-25"
-    });
+    })
+    it('should generate correct number of periods', () => {
+        expect(periods.length).toEqual(monthlyDates.length)
+    })
     for (const periodTest of monthlyDates) {
         it(`should generate correct period for the period ${periodTest.name}`, () => {
-            const selectedPeriod = periods.find((period)=> period.id === periodTest.id);
-            expect(selectedPeriod).toBeDefined();
-            if(selectedPeriod) {
-                expect(selectedPeriod.displayName).toEqual(periodTest.name);
-                expect(selectedPeriod.startDate).toEqual(periodTest.startDate);
-                expect(selectedPeriod.endDate).toEqual(periodTest.endDate);
+            const selectedPeriod = periods.find(
+                (period) => period.id === periodTest.id
+            )
+            expect(selectedPeriod).toBeDefined()
+            if (selectedPeriod) {
+                expect(selectedPeriod.displayName).toEqual(periodTest.name)
+                expect(selectedPeriod.startDate).toEqual(periodTest.startDate)
+                expect(selectedPeriod.endDate).toEqual(periodTest.endDate)
             }
         })
     }
-
 })
+
+const dailyDates = []
